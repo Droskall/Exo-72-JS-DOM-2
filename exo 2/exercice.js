0a50980a1579cc4a3d5f6a1beb339d416de8b9ff
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-18 09:30:57 +0100

    finish

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,3 +1,7 @@
+let elements = document.getElementsByTagName('div');
+for (let i in elements){
+    elements.item(i).innerHTML = "Bonjour je suis"
+}
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,3 +1,15 @@
+let div = document.getElementsByClassName('name-tag');
+let div2 = document.getElementsByClassName('name-tag');
+let newP = document.createElement('p');
+newP.innerHTML = "HELLO WORLD";
+
+for (let i in div){
+    div.item(i).appendChild(newP);
+}
+
+for (let y in div2){
+    div.item(y).appendChild(newP);
+}
 
 
 
